fc089c4620992708ba505e5852d2c7f3075c7514
kedriss <cedric.linque@gmail.com>
2017-01-28 15:19:20 +0100

    adding security

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,4 +1,20 @@
 ////// DRAP N DROP/////////////
+var token;
+
+var checktoken= function(){
+    return new Promise( function(resolve,reject){
+        if(token)
+            resolve(token)
+        else{ // show connection
+            showConnection();
+            reject(new Error("pas de token"));
+        }
+});
+}
+
+function showConnection() {
+    $("#connection").modal();
+}
 function allowDrop(ev) {
     ev.preventDefault();
 }
@@ -32,6 +48,31 @@ function drop(ev, id) {
     $('#trash').hide(500);
 }
 /////////// FIN DRAG N DROP////////////
+
+///////////// COOKIE//////////////
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+///////////////FIN COOKIE//////////////////
 function remove(nom,skipIt){
     let objet;
     var infos =[];
@@ -60,75 +101,99 @@ function remove(nom,skipIt){
     if(objet && !skipIt)_deleted.push(objet);
     return objet;
 }
-$(function(){
+$(function() {
 
-        $('#trash').hide();
+    token = getCookie('token');// recuperation du potentiel token enregistré en cookie
+    $('#trash').hide();
 
-        $('#aModifier').change(function () {
-            var nom = $('#aModifier').val();
-            if(nom) {
-                var info = nom.split(';');
-                $.ajax("/lien/" + info[0] + "/" + info[1]).then(function (link, err) {
-                    $("#nom").val(link.nom);
-                    $("#liens").val(link.liens);
-                    $("#image").val(link.image);
-                    $("#sequence").val(link.sequence);
+    $('#aModifier').change(function () {
+        var nom = $('#aModifier').val();
+        if (nom) {
+            var info = nom.split(';');
+            $.ajax("/lien/" + info[0] + "/" + info[1]).then(function (link, err) {
+                $("#nom").val(link.nom);
+                $("#liens").val(link.liens);
+                $("#image").val(link.image);
+                $("#sequence").val(link.sequence);
 
-                });
-            }else {
-                $("#nom").val('');
-                $("#liens").val('');
-                $("#image").val('');
-                $("#sequence").val('');
-            }
+            });
+        } else {
+            $("#nom").val('');
+            $("#liens").val('');
+            $("#image").val('');
+            $("#sequence").val('');
+        }
 
-        })
+    })
 
-        $('#Appliquer').click(function(){
+    $('#Appliquer').click(function () {
+        checktoken().then(function (token) {
             var tomodify = $("#aModifier").val();
             var _id;
-            if (tomodify) _id=tomodify.split(";")[1];
-            var nom   = $("#nom").val();
+            if (tomodify) _id = tomodify.split(";")[1];
+            var nom = $("#nom").val();
             var liens = $("#liens").val();
             var image = $("#image").val();
-            var cote  = $("#cote").val();
-            var order  = $("#sequence").val()?$("#sequence").val():0;
-            if(nom) {
-                if(_id){// modification
-                    var newvar = {_id:_id,sequence: order, nom: nom, image: image, liens: liens, side:cote};
-                }else
-                {// creation
-                    var newvar = {sequence: order, nom: nom, image: image, liens: liens,side:cote};
+            var cote = $("#cote").val();
+            var order = $("#sequence").val() ? $("#sequence").val() : 0;
+            if (nom) {
+                if (_id) {// modification
+                    var newvar = {_id: _id, sequence: order, nom: nom, image: image, liens: liens, side: cote,token:token};
+                } else {// creation
+                    var newvar = {sequence: order, nom: nom, image: image, liens: liens, side: cote,token:token};
                 }
                 $.ajax({
-                        url:"/lien",
-                        data:newvar,
-                        type:'PUT'
-                    })
+                    url: "/lien",
+                    data: newvar,
+                    type: 'PUT',
+                    success:function(){ location.reload();}
+                })
 
 
             }
-        })
+        });
+    })
 
-    $('#Supprimer').click(function(){
+    $('#Supprimer').click(function () {
         var tomodify = $("#aModifier").val();
         var _id;
         var side;
-        if (tomodify){
+        checktoken().then(function(token){
+        if (tomodify) {
             _id = tomodify.split(";")[1];
             side = tomodify.split(";")[0];
 
 
-            var params = {_id:_id, cote:side};
+            var params = {_id: _id, cote: side, token:token};
             $.ajax({
-                url:"/lien",
-                data:params,
-                type:'DELETE'
+                url: "/lien",
+                data: params,
+                type: 'DELETE'
             })
 
 
         }
+        });
     })
+
+$('#connectionButton').click(function(){
+    var username = $('#username').val();
+    var pwd=$('#password').val();
+    var params = {username: username, password: pwd};
+
+    $.ajax({
+        url: "/auth",
+        data: params,
+        type: 'POST',
+        success:function(data){
+            token = data.token;
+            setCookie('token',token,1);
+            if(token){
+                $('#connection').modal('toggle');
+            }
+        }
+    })
+})
 
     }
 
--- routes/index.js
+++ routes/index.js
@@ -7,25 +7,58 @@ var MongoClient = require('mongodb').MongoClient,
 var coteOppose ={"gaucheCenter":"droiteCenter", "droiteCenter":'gaucheCenter'};
 var uriMongo = "mongodb://heroku_t9m2n7qd:obvtjtruvded5pmam57m3542b5@ds127978.mlab.com:27978/heroku_t9m2n7qd";
 //var uriMongo="mongodb://localhost:27017/userLinks";
-/* GET home page. */
+
+var checktoken = function(token){
+
+    return new Promise(function(resolve,reject){
+        MongoClient.connect(uriMongo,function(err, db) {
+            var users = db.collection('users');
+            users.findOne({token:token}).then(function(user){
+                resolve(user);
+            })
+        });
+        });
+}
 router.get('/', function(req, res, next) {
     console.log("route = /");
     var result;
     var gaucheCenter=[];
     MongoClient.connect(uriMongo,function(err, db) {
-
-        if(!err) {
+        if (!err) {
             console.log("We are connected");
-            //console.log(db);
-        }
+
         var collection = db.collection("gaucheCenter");
-        collection.find({}).sort({sequence:1}).toArray().then(function(gaucheCenter){
+        collection.find({}).sort({sequence: 1}).toArray().then(function (gaucheCenter) {
             var collection = db.collection("droiteCenter");
-            collection.find({}).sort({sequence:1}).toArray(function(e,result){
-                res.render('index', { title: 'Express',gaucheCenter: gaucheCenter,droiteCenter:result });
+            collection.find({}).sort({sequence: 1}).toArray(function (e, result) {
+                res.render('index', {title: "Salut l'artiste", gaucheCenter: gaucheCenter, droiteCenter: result});
             });
         })
+    }
+    });
 
+});
+
+router.post('/auth', function(req, res, next) {
+    var username = req.body.username;
+    var pwd = req.body.password;
+    console.log(req.params);
+    console.log(username);
+    console.log(pwd);
+    MongoClient.connect(uriMongo,function(err, db) {
+        if (!err) {
+            console.log("We are connected");
+
+            var collection = db.collection("users");
+            collection.findOne({username:username,password:pwd}).then(function (user) {
+                if (user ){
+                    console.log(user);
+                    res.json({token:user.token});
+                }
+                res.json({token:null});
+
+            })
+        }
     });
 
 });
@@ -55,30 +88,34 @@ router.put('/lien', function(req, res, next) {
     var image = lien.image;
     var sequence = lien.sequence;
     var nom = lien.nom;
+    var token = lien.token;
     var cote = lien.side;
-    MongoClient.connect(uriMongo,function(err, db) {
-        var collection = db.collection(cote);
-        var collectionOpposite = db.collection(coteOppose[cote]);
-        if (_id) {
-            var o_id = new ObjectId(_id);
-            if (collection){
-                collection.find({_id:o_id}).toArray().then(function(data) {
-                    if (data && data.length > 0) {
-                        collection.updateOne({_id: o_id}, {nom: nom, liens: url, image: image, sequence: sequence});
-                    }
-                    else if(collectionOpposite) { // SI on a pas trouver sur le cote, c'est qu'il faut le supprimer sur le coté opposé et creer dans le coté
-                            collectionOpposite.deleteOne({_id:o_id});
-                            collection.insertOne({nom:nom,liens:url, image:image,sequence:sequence});
+    checktoken(token).then(function(){
+        MongoClient.connect(uriMongo,function(err, db) {
+            var collection = db.collection(cote);
+            var collectionOpposite = db.collection(coteOppose[cote]);
+            if (_id) {
+                var o_id = new ObjectId(_id);
+                if (collection){
+                    collection.find({_id:o_id}).toArray().then(function(data) {
+                        if (data && data.length > 0) {
+                            collection.updateOne({_id: o_id}, {nom: nom, liens: url, image: image, sequence: sequence});
                         }
-            })
-            }
-            }
-            else{
-                if(collection)
-                    collection.insertOne({nom: nom, liens: url, image: image, sequence: sequence});
-            }
-    })
-    res.json({});
+                        else if(collectionOpposite) { // SI on a pas trouver sur le cote, c'est qu'il faut le supprimer sur le coté opposé et creer dans le coté
+                                collectionOpposite.deleteOne({_id:o_id});
+                                collection.insertOne({nom:nom,liens:url, image:image,sequence:sequence});
+                            }
+                })
+                }
+                }
+                else{
+                    if(collection)
+                        collection.insertOne({nom: nom, liens: url, image: image, sequence: sequence});
+                }
+            res.json({});
+        })
+    });
+
 });
 
 router.delete('/lien', function(req, res, next) {
